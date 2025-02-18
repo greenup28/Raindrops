722c0280516797f0c1e03f7f1ecc825d9820922b
greenup28 <greenup28@up.edu>
2025-02-17 20:27:34 -0800

    Final form of this project.

--- app/src/main/java/edu/up/raindrops/DROPS.java
+++ app/src/main/java/edu/up/raindrops/DROPS.java
@@ -16,6 +16,7 @@ import android.graphics.Canvas;
 import android.graphics.Color;
 import android.graphics.Paint;
 import android.util.AttributeSet;
+import android.util.Log;
 import android.view.SurfaceView;
 
 import androidx.core.graphics.ColorUtils;
@@ -43,12 +44,19 @@ public class DROPS extends SurfaceView {
 
     //Array to hold all of the colors, so when the loop is called, this can cycle through the colors
     Paint[] colors = new Paint[12];
+    //Holds the strings of each of the colors to be used when it is blended
+    String[] stringColor = new String[12];
 
     //The main raindrop variables
     int mainDrop;
     float mainX = randomPositionX();
     float mainY = randomPositionY();
+    Paint mainColor;
+    String mainColorString;
     int mainBlendedColor;
+
+    //Sees if the drop is deleted or not
+    Boolean[] notDeleted = new Boolean[12];
 
     //Arrays to hold the x and y positions of the other drops.
     //If I don't have these, the drops get redrawn in a different position everytime the main drops is moved.
@@ -73,10 +81,15 @@ public class DROPS extends SurfaceView {
 
         //sets the number of rain drops drawn, new object in order to use the random class
 
-        numberOfDrops = (float) (Math.random()*7) + 5 ;
+        numberOfDrops = (float) ((Math.random()*7) + 5) ;
 
         //selects the main raindrop
         selectMainDrop();
+
+        //sets all the drops to be drawn
+        for(int i = 0; i < numberOfDrops; i++){
+            notDeleted[i] = true;
+        }
 
         //selects the random x and y positions for the other drops
         for(int i = 0; i < numberOfDrops; i++){
@@ -93,42 +106,55 @@ public class DROPS extends SurfaceView {
         //Initialize drawing styles
         _blue.setColor(0xFF14EFFF); //Light Blue
         _blue.setStyle(Paint.Style.FILL);
+        stringColor[0] = "#14EFFF";
 
         _white.setColor(0xFFD7FAFC); //White Blue
         _white.setStyle(Paint.Style.FILL);
 
+
         _darkBlue.setColor(0xFF00548C); //Dark Blue
         _darkBlue.setStyle(Paint.Style.FILL);
+        stringColor[1] = "#00548C";
 
         _seaBlue.setColor(0xFF409FDE); //Sea Blue
         _seaBlue.setStyle(Paint.Style.FILL);
+        stringColor[2] = "#409FDE";
 
         _skyBlue.setColor(0xFF52D8F2); //Sky Blue
         _skyBlue.setStyle(Paint.Style.FILL);
+        stringColor[3] = "#52D8F2";
 
         _snowBlue.setColor(0xFF83D6EB); //Snow Blue
         _snowBlue.setStyle(Paint.Style.FILL);
+        stringColor[4] = "#83D6EB";
 
         _rainBlue.setColor(0xFF0F9CBF); //Rain Blue
         _rainBlue.setStyle(Paint.Style.FILL);
+        stringColor[5] = "#0F9CBF";
 
         _bluerBlue.setColor(0xFF012B36); //Bluer Blue
         _bluerBlue.setStyle(Paint.Style.FILL);
+        stringColor[6] = "#012B36";
 
         _notBlue.setColor(0xFF2900F7); //Not Blue
         _notBlue.setStyle(Paint.Style.FILL);
+        stringColor[7] = "#2900F7";
 
         _blueGreen.setColor(0xFF7CF7D6); // Green Blue
         _blueGreen.setStyle(Paint.Style.FILL);
+        stringColor[8] = "#7CF7D6";
 
         _justBlue.setColor(0xFF00DDFF); // Just Blue
         _justBlue.setStyle(Paint.Style.FILL);
+        stringColor[9] = "#00DDFF";
 
         _redSeaBlue.setColor(0xFFF5586D); // Red Sea Blue
         _redSeaBlue.setStyle(Paint.Style.FILL);
+        stringColor[10] = "#F5586D";
 
         _jeanBlue.setColor(0xFF0B3CB8); //Jean Blue
         _jeanBlue.setStyle(Paint.Style.FILL);
+        stringColor[11]= "#0B3CB8";
 
         //sets up the array so each place is filled with a different color
         colors[0] = _blue;
@@ -164,8 +190,12 @@ public class DROPS extends SurfaceView {
 
             if(i == mainDrop){
                 drawMainDrop(rain, colors[i]);
+                mainColor = colors[i];
+                mainColorString = stringColor[i];
             }else {
-                drawRainDrop(rain, dropsX[i], dropsY[i], colors[i]);
+               if(notDeleted[i] == true) {
+                   drawRainDrop(rain, dropsX[i], dropsY[i], colors[i]);
+               }
             }
 
         }
@@ -173,39 +203,79 @@ public class DROPS extends SurfaceView {
 
     }
 
+    /**
+     * This checks if the main drop is touching any other drops.
+     * This does so by using the x and the y's each drop.
+     *
+     * No parameters
+     *
+     * Returns nothing.
+     */
     public void checkTouchingDrops(){
-        //TODO write this method and figure out how to use the blended color
+
         for(int i = 0; i < numberOfDrops; i++){
 
-            //Right of the main drop and left of the drop being checked
-            if(((mainX + 30) >= dropsX[i]) && ((mainX + 30) <= (dropsX[i] + 30))){
+            //Makes this ignore the main drop so it isn't blending itself
+            if(i == mainDrop){
+
+            }
+             //Right of the main drop and left of the drop being checked
+             //Then there is also the end bound, I do this for the rest as well
+            else if((((mainX + 30) >= dropsX[i]) && ((mainX + 30) <= (dropsX[i] + 30)))){
+                //Top of the main drop and bottom of the drop being checked
+                if(((mainY <= (dropsY[i] + 30)) && (mainY >= dropsY[i]))){
+
+                    blendDropColor(i);
+                    deleteDrops(i);
+                }
 
             }
             //Left of the main drop and right of the drop being checked
             else if((mainX <= (dropsX[i] + 30)) && (mainX >= dropsX[i])){
+                //Bottom of the main drop and top of the drop being checked
+                if(((mainY + 30) >= dropsY[i]) && ((mainY + 30) <= (dropsY[i] + 30))){
+                    blendDropColor(i);
+                    deleteDrops(i);
+
+                }
 
             }
-            //Bottom of the main drop and top of the drop being checked
-            else if(((mainY + 30) >= dropsY[i]) && ((mainY + 30) >= (dropsY[i] + 30))){
 
-            }
-            //Top of the main drop and bottom of the drop being checked
-            else if(((mainY <= (dropsY[i] + 30)) && (mainY <= dropsY[i]))){
 
-            }
+
         }
 
-        //blendDropColor();
+
     }
 
-    public void blendDropColor(Paint color1, Paint color2){
+    public void deleteDrops(int i){
+        notDeleted[i] = false;
+    }
+
+    /**
+     * This blends the colors.
+     * First it converts them to ints as the hex is a string and you can't do anything with those.
+     * Then the ints are blended together, set back to hex, and sets the main color.
+     *
+     * This was unnecessarily hard and I had to consult the internet so many times that most of the
+     * times it was not helpful. I wanted to cry.
+     *
+     * No parameters
+     *
+     * Returns nothing.
+     */
+    public void blendDropColor(int i){
 
         //Had to convert the hex code from a string to int.
-        int firstColor = Color.parseColor(String.valueOf(color1));
-        int secondColor = Color.parseColor(String.valueOf(color2));
+
+        int firstColor = Color.parseColor(mainColorString);
+        int secondColor = Color.parseColor(stringColor[i]);
 
         //This only took ints so could not use the hex colors.
         mainBlendedColor = ColorUtils.blendARGB(firstColor, secondColor, 0.5F);
+
+        mainColor.setColor(mainBlendedColor);
+
     }
 
     //These getters are here so I can use these values in the controller and set the seekbar to this
@@ -231,13 +301,13 @@ public class DROPS extends SurfaceView {
     //when called determines the random position X for each raindrop
     public float randomPositionX(){
 
-        return (float) Math.random()*1450;
+        return (float) Math.random()*801;
     }
 
     //when called determines the random position Y for each raindrop
     public float randomPositionY(){
 
-        return (float) Math.random()*1050;
+        return (float) Math.random()*801;
     }
 
     //draws the raindrop with a diameter of 30 dp and a highlight of 10 dp
--- app/src/main/java/edu/up/raindrops/RaindropController.java
+++ app/src/main/java/edu/up/raindrops/RaindropController.java
@@ -1,3 +1,11 @@
+/**
+ * This is the controller that acts as a go between of the seekbar and the surface view.
+ *
+ * @author Madilynn Greenup
+ * @date 2/9/25
+ *
+ */
+
 package edu.up.raindrops;
 
 import android.os.Bundle;
@@ -18,16 +26,30 @@ public class RaindropController implements SeekBar.OnSeekBarChangeListener{
 
     }
 
+    /**
+     * Communicated with both MainActivity and DROPS as it gets the progress and moved the main drop.
+     * It also makes sure that it is user input before changing anything.
+     *
+     * @param seekBar that checks which of the two seekBars is being used
+     * @param progress is the position of the seek bar
+     * @param fromUser checks is if it is the starting position or changed from the user
+     *
+     * Returns nothing
+     */
     @Override
     public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
         if(seekBar == seekBar.findViewById(R.id.UpandDown)){
             _rain.mainY = progress;
-            _rain.checkTouchingDrops();
+            if(fromUser == true) {
+                _rain.checkTouchingDrops();
+            }
             _rain.invalidate();
         }
         if(seekBar == seekBar.findViewById(R.id.SidetoSide)){
             _rain.mainX = progress;
-            _rain.checkTouchingDrops();
+            if(fromUser == true) {
+                _rain.checkTouchingDrops();
+            }
             _rain.invalidate();
         }
     }
